97fec4e06da564401a122cdccb35c9fe031339c4
alifia putri ramdhani <alifiaputri.ramadhani@gmail.com>
2017-01-31 13:42:30 +0700

    Final Commit

---
+++ asp_mvc_2/Models/EntityManager/DemoDBEntities.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp_mvc_2.Models.EntityManager
+{
+    internal class DemoDBEntities
+    {
+        public object SYSUserProfiles { get; internal set; }
+        public object SYSUsers { get; internal set; }
+    }
+}
