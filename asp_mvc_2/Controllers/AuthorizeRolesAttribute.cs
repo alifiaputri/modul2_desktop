438f33fb9ddbde2a4df078e857b1301b6fed879b
alifia putri ramdhani <alifiaputri.ramadhani@gmail.com>
2017-02-20 08:48:35 +0700

    modul3

--- asp_mvc_2/Controllers/AuthorizeRolesAttribute.cs
+++ asp_mvc_2/Controllers/AuthorizeRolesAttribute.cs
@@ -4,5 +4,11 @@ namespace asp_mvc_2.Controllers
 {
     internal class AuthorizeRolesAttribute : Attribute
     {
+        private string v;
+
+        public AuthorizeRolesAttribute(string v)
+        {
+            this.v = v;
+        }
     }
 }
